d6d4842956a5dfe1549127ab9884bc48c78f3988
fujiington <luno5@elev.techcollege.dk>
2026-01-09 12:59:47 +0100

    fuckass fixes

--- server/index.ts
+++ server/index.ts
@@ -11,7 +11,6 @@ export function createServer() {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  // Example API routes
   app.get("/api/ping", (_req, res) => {
     const ping = process.env.PING_MESSAGE ?? "ping";
     res.json({ message: ping });
